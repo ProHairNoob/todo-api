fe3c00efe0b2e7ec10a99e1636964e117e848a0b
Yakoub <yakoubbeneyad@gmail.com>
2026-09-01 22:28:46 +0200

    added password and email authentication

--- register.py
+++ register.py
@@ -1,3 +1,4 @@
+import bcrypt
 from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel, EmailStr
 
@@ -36,6 +37,7 @@ def register_user(user: User):
         (user.username, user.email, password),
     )
     conn.commit()
+    # debug
     cursor.execute("SELECT * FROM users")
     users = cursor.fetchall()
     counter = 0
@@ -45,3 +47,25 @@ def register_user(user: User):
 
     conn.close()
     return "User registered successfully"
+
+
+@router.post("/login")
+def user_login(user: User):
+    # check if password is valid
+    # check if email is valid
+    conn = connect_users_db()
+    cursor = conn.cursor()
+    print(user.email)
+    cursor.execute("SELECT password FROM users WHERE email = ?", (user.email,))
+    row = cursor.fetchone()
+    print(row, hash_password(user.password))
+    conn.close()
+    if not row:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid email or password"
+        )
+    if not bcrypt.checkpw(user.password.encode("utf-8"), row[0]):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid email or password"
+        )
+    return "login succesful"
