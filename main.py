c0a67b12a1ae8a5882b056a7f7bff971c70cdad0
Yakoub <yakoubbeneyad@gmail.com>
2026-09-09 04:06:47 +0200

    changed uvicorn workers to default instead of 4

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ if __name__ == "__main__":
         "main:app",
         host="0.0.0.0",
         port=8000,
-        workers=4,
         reload=False,
         log_level="warning",
         access_log=False,
